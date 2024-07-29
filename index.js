e3a6f4086d6704343c43c0fcc49ae647c7bef069
gomezjeswel <gomezjeswel@gmail.com>
2024-07-29 11:38:56 +0800

    Refactor corps option

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["capstone3-three-navy.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
