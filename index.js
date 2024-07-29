dc8ae89b65af14d21ab0ee83033a203408ab7a74
rowimaytamayo <rowimaytamayo@gmail.com>
2024-07-29 12:55:51 +0800

    Merge branch 'main' of ec2-54-147-90-61.compute-1.amazonaws.com:B442/csp2-b442-gomez-tamayo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const userRoutes = require("./routes/user.js");
-const productRoutes = require("./routes/product.js");
-const cartRoutes = require("./routes/cart.js");
-const orderRoutes = require("./routes/order.js");
-const port = 4000;
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const userRoutes = require("./routes/user.js")
+const productRoutes = require("./routes/product.js")
+const cartRoutes = require("./routes/cart.js")
+const orderRoutes = require("./routes/order.js")
+const port = 4000
 require("dotenv").config()
 
 const app = express()
@@ -14,18 +14,25 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const corsOptions = {
-  origin: ["capstone3-three-navy.vercel.app", "capstone3-git-master-jeswels-projects.vercel.app", "capstone3-budc27coh-jeswels-projects.vercel.app"],
+  origin: [
+    "capstone3-xi.vercel.app",
+    "https://capstone3-git-master-rowies-projects.vercel.app/",
+    "https://capstone3-git-master-rowies-projects.vercel.app/",
+  ],
   credentials: true,
   optionsSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions))
 
-mongoose.connect(process.env.MONGODB_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-app.use("/b1/users", userRoutes);
-app.use("/b1/products", productRoutes);
-app.use("/b1/cart", cartRoutes);
-app.use("/b1/orders", orderRoutes);
+mongoose.connect(process.env.MONGODB_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+app.use("/b1/users", userRoutes)
+app.use("/b1/products", productRoutes)
+app.use("/b1/cart", cartRoutes)
+app.use("/b1/orders", orderRoutes)
 
 app.listen(port, () =>
   console.log(`API is now available on port ${process.env.PORT || port}`)
